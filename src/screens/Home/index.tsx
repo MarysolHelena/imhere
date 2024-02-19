5ab41d05378c14937a383f2926a54ef1a544ccf5
MarysolHelena <marysolgomesm@gmail.com>
2024-02-19 14:06:37 -0300

    init

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -51,7 +51,7 @@ export function Home() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventName}>Evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de novembro de 2024</Text>
 
       <View style={styles.form}>
